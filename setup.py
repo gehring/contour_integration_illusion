f8c665d86efb5d839a7ca22432b997922c376e37
LIS <gehring@mit.edu>
2019-05-08 20:38:59 -0400

    removed experiment from notebook for now

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="demo-contour-pkg",
+    version="0.0.1",
+    packages=setuptools.find_packages(),
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
